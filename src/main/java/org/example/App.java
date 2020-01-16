cba333dbcca3b2e913beea27e18e2a72697ab786
FinalDayz <steef2000d@gmail.com>
2020-01-16 09:36:09 +0100

    added experimentDetails

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,12 +8,11 @@ import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.example.auth.AuthenticationFilter;
+import org.example.model.ExperimentDetails;
 import org.example.persistence.ExperimentDAO;
+import org.example.persistence.ExperimentDetailsDAO;
 import org.example.persistence.UserDAO;
-import org.example.service.ExperimentService;
-import org.example.service.LoginService;
-import org.example.service.RegisterService;
-import org.example.service.UserService;
+import org.example.service.*;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.skife.jdbi.v2.DBI;
 
@@ -70,7 +69,9 @@ public class App extends Application<AppConfiguration> {
             protected void configure() {
                 bind(jdbi.onDemand(UserDAO.class)).to(UserDAO.class);
                 bind(jdbi.onDemand(ExperimentDAO.class)).to(ExperimentDAO.class);
+                bind(jdbi.onDemand(ExperimentDetailsDAO.class)).to(ExperimentDetailsDAO.class);
                 bind(LoginService.class).to(LoginService.class);
+                bind(ExperimentDetailsService.class).to(ExperimentDetailsService.class);
                 bind(RegisterService.class).to(RegisterService.class);
                 bind(UserService.class).to(UserService.class);
                 bind(ExperimentService.class).to(ExperimentService.class);
---
+++ src/main/java/org/example/resource/ExperimentDetailsResource.java
@@ -0,0 +1,49 @@
+package org.example.resource;
+
+import org.example.model.ExperimentDetails;
+import org.example.service.ExperimentDetailsService;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Singleton
+@Path("/experimentDetails")
+@Produces(MediaType.APPLICATION_JSON)
+public class ExperimentDetailsResource {
+
+    private final ExperimentDetailsService service;
+
+    @Inject
+    public ExperimentDetailsResource(ExperimentDetailsService service) {
+        this.service = service;
+    }
+
+    @GET
+    @Path("/{id}")
+    public ExperimentDetails retrieve(@PathParam("id") int id)
+    {
+        return service.find(id);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public void delete(@PathParam("id") int id)
+    {
+        service.delete(id);
+    }
+
+    @POST
+    @Path("/")
+    public int insert(ExperimentDetails experimentDetails)
+    {
+        return service.add(experimentDetails);
+    }
+
+    @PUT
+    @Path("/{id}")
+    public void update(@PathParam("id") int id, ExperimentDetails experimentDetails)
+    {
+        service.update(id, experimentDetails);
+    }
+}
