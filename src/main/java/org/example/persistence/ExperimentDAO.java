f32c24328ffdb588f0ca7e3ed6c3d5c6c993ea32
1115487 <s1115487@student.hsleiden.nl>
2020-01-28 10:31:02 +0100

    Beschrijving refactor naar experiment + inovatie > innovatie

--- src/main/java/org/example/persistence/ExperimentDAO.java
+++ src/main/java/org/example/persistence/ExperimentDAO.java
@@ -27,19 +27,19 @@ public interface ExperimentDAO {
     @SqlQuery( "SELECT LAST_INSERT_ID()")
     public int getLastID();
 
-    @SqlQuery("SELECT experiment_naam, experiment_leider_primair, experiment_leider_secundair, fase ,wijziging_datum, experiment_ID, status_kleur FROM experiment ORDER BY <attribute> <order>;")
+    @SqlQuery("SELECT experiment_naam, experiment_leider_primair, experiment_leider_secundair, fase ,wijziging_datum, experiment_ID, status_kleur, beschrijving FROM experiment ORDER BY <attribute> <order>;")
     @Mapper(ExperimentMapper.class)
     public List<Experiment> orderBy(@Define("attribute") String attribute, @Define("order") String order);
 
     //--------------------FILTERS--------------------
 
-    @SqlQuery("SELECT experiment_naam, experiment_leider_primair, experiment_leider_secundair, fase ,wijziging_datum, experiment_ID, status_kleur FROM experiment WHERE <filter> = :value ;")
+    @SqlQuery("SELECT experiment_naam, experiment_leider_primair, experiment_leider_secundair, fase ,wijziging_datum, experiment_ID, status_kleur, beschrijving FROM experiment WHERE <filter> = :value ;")
     @Mapper(ExperimentMapper.class)
     public List<Experiment> filter(@Define("filter") String filter, @Bind("value") String value);
 
     //--------------------ARCHIVE--------------------
 
-    @SqlQuery("SELECT experiment_naam, experiment_leider_primair, experiment_leider_secundair, fase ,wijziging_datum, experiment.experiment_ID, experiment.status_kleur FROM experiment INNER JOIN experiment_details ON experiment.experiment_ID=experiment_details.experiment_ID\n" +
+    @SqlQuery("SELECT experiment_naam, experiment_leider_primair, experiment_leider_secundair, fase ,wijziging_datum, experiment.experiment_ID, experiment.status_kleur, experiment.beschrijving FROM experiment INNER JOIN experiment_details ON experiment.experiment_ID=experiment_details.experiment_ID\n" +
             "WHERE archief_type = :type;")
     @Mapper(ExperimentMapper.class)
     public List<Experiment> filterArchive(@Bind("type") String type);
@@ -47,7 +47,7 @@ public interface ExperimentDAO {
     //--------------------SEARCH--------------------
 
     //Filter search bar
-    @SqlQuery("SELECT experiment_naam, experiment_leider_primair, experiment_leider_secundair, fase, wijziging_datum, experiment_ID, status_kleur FROM experiment WHERE experiment_naam LIKE %:searchString% OR experiment_leider_primair LIKE %:searchString% OR experiment_leider_secundair LIKE %:searchString%;")
+    @SqlQuery("SELECT experiment_naam, experiment_leider_primair, experiment_leider_secundair, fase, wijziging_datum, experiment_ID, status_kleur, beschrijving FROM experiment WHERE experiment_naam LIKE %:searchString% OR experiment_leider_primair LIKE %:searchString% OR experiment_leider_secundair LIKE %:searchString%;")
     @Mapper(ExperimentMapper.class)
     public List<Experiment> filterSearch(@Bind("searchString")String searchString);
 
@@ -64,8 +64,8 @@ public interface ExperimentDAO {
     @SqlUpdate("DELETE FROM experiment WHERE experiment_ID = :id")
     public void delete(@Bind("id")int id);
 
-    @SqlUpdate("INSERT INTO experiment (experiment_naam, wijziging_datum, fase, experiment_leider_primair, experiment_leider_secundair, status_kleur) " +
-            "VALUES (:experiment_naam, :wijziging_datum, :fase, :experiment_leider_primair, :experiment_leider_secundair, :color);")
+    @SqlUpdate("INSERT INTO experiment (experiment_naam, wijziging_datum, fase, experiment_leider_primair, experiment_leider_secundair, status_kleur, beschrijving) " +
+            "VALUES (:experiment_naam, :wijziging_datum, :fase, :experiment_leider_primair, :experiment_leider_secundair, :color, :beschrijving);")
     public void add(@BindBean Experiment newExperiment);
 
     @SqlUpdate("UPDATE experiment SET " +
@@ -75,6 +75,7 @@ public interface ExperimentDAO {
             "status_kleur=:color," +
             "experiment_leider_primair = :experiment_leider_primair, " +
             "experiment_leider_secundair = :experiment_leider_secundair " +
+            "beschrijving = :beschrijving" +
             "WHERE experiment_ID = :id")
     public void update(@Bind("id") int id, @BindBean Experiment updatedExperiment);
 
